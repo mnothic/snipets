ab3ddc06f562f3a16eb29caec69bdb20cc82179c
theManda <jorge@bsdchile.cl>
2013-05-30 15:38:55 +0200

    Update clean_mpath_devices_rhel.sh

--- clean_mpath_devices_rhel.sh
+++ clean_mpath_devices_rhel.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# quita los discos desde la cabina
+# quitar los discos desde la cabina
 # y luego ejecuta el script
 # https://access.redhat.com/site/documentation/en-US/Red_Hat_Enterprise_Linux/5/html/Online_Storage_Reconfiguration_Guide/removing_devices.html
 
 for i in $(multipath |grep "tur checker reports path is down" |awk '{print $1}' |tr -d ":")
 do 
-  multipath -f /dev/$i;
-	blockdev -flushbufs /dev/$i; 
-	echo 1 > /sys/block/${i}/device/delete; 
-	rm -rf /dev/${i};
+	multipath -f /dev/$i > /dev/null 2>&1;
+	blockdev -flushbufs /dev/$i > /dev/null 2>&1; 
+	echo 1 > /sys/block/${i}/device/delete > /dev/null 2>&1; 
+	rm -rf /dev/${i} > /dev/null 2>&1;
 done
