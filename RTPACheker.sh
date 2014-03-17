c462116a76175a319521153450921657197614b1
theManda <mnothic@users.noreply.github.com>
2014-03-17 16:37:55 +0100

    Update RTPACheker.sh

--- RTPACheker.sh
+++ RTPACheker.sh
@@ -1,22 +1,27 @@
-#!/usr/bin/env bash
-#: Title : RTPA GNI Checker
+#!/usr/bin/env [ba|k]sh
+#: Title : RTPA HP Checker
 #: Date : 2013-07-24
 #: Author : "Jorge Medina" <jmedina@hp.com>
 #: Version : 1.0
 #: Description : Check RTPA from chekcklist.csv
 #: Options : ./rtpa.sh [ -h -f checklist.csv ]
 #: NOTES:
-# install in /home/gni/bin
-# RTPA CHECKER
-# works in AIX and LINUX
-############################
-BASEDIR='/home/gni/bin'
-CHECKLIST=$BASEDIR'/checklist.csv'
-usage()
-{
-	echo "$0 [-f file.csv]"
-	exit 0
-}
+#: install in $BASEDIR and download https://github.com/mnothic/nshell
+#: RTPA CHECKER
+#: works in AIX and LINUX
+#:
+BASEDIR='./'
+. $BASEDIR/nshell.sh
+
+##
+# this show the options of the script
+##
+#:
+#: here we parsing options flags
+#: loop all shell args.
+
+CHECKLIST="${BASEDIR}/$(lower $(get_os_name))_checklist.csv"
+
 while getopts f:h opt
 do
 	case $opt in
@@ -27,85 +32,43 @@ do
 	esac
 done
 
-exist()
-{
-	if [[ -f '$1' ]]; then
-		echo 'PASS'
-	else
-		echo 'FAIL'
-	fi
-}
-
-running()
-{
-	ret=$(ps auxw |grep -i $pattern |grep -v grep|wc -l |tr -d ' ')
-	if [[ $ret -gt 0 ]]; then
-		echo 'PASS'
-	else
-		echo 'FAIL'
-	fi
-}
-
-search()
-{
-	if [ -f $2 ]; then
-		grep -v "^#" $2 | grep -i "$1" > /dev/null 2>&1 
-		if [[ $? -eq 0 ]]; then
-			echo 'PASS'
-		else
-			echo 'FAIL'
-		fi
-	else
-		echo 'FAIL'
-	fi
-}
-
-if [ ! -f $CHECKLIST ]; then
+if [ $(file_exist $CHECKLIST) == 'NOT' ]; then
 	echo $CHECKLIST" not exist"
-	exit 0
+    usage "[-f file.csv]"
 fi
-
+#:
+#: here is where magic occurs
+#: we deal with all options in csv
+#:
 cat $CHECKLIST |while read line
 do
+	#
+	# here parsing the csv and tokenize it
+	#
 	type=$(echo $line | awk '{split($0, a, ";");print a[1]}')
 	desc=$(echo $line | awk '{split($0, a, ";");print a[2]}')
 	pattern=$(echo $line | awk '{split($0, a, ";");print a[3]}')
 	file=$(echo $line | awk '{split($0, a, ";");print a[4]}')
-
 	case $type in
 		'config')
-			pattern="${pattern%"${pattern##*[![:space:]]}"}"
-			status=$(search "$pattern" "$file")
+			status=$(find_string_in_file "$pattern" "$file")
 			;;
 		'process')
 			status=$(running "$pattern")
 			;;
 		'file')
-			status=$(exist "$pattern")
+			status=$(file_exist "$pattern")
 			;;
 	esac
-	if [[ $type == 'config' || $type == 'process' || $type == 'file' ]];then 
-		len=${#desc}
-		dif=$(expr 80 - $len)
-		x=0
-		spaces=""
-		while [ $x -lt $dif ]
-		do
-			x=$(expr $x + 1)
-			spaces="$spaces "
-		done
-		if [ $(uname -s) == 'Linux' ]; then
-			if [ $status == 'FAIL' ]; then
-				echo -n $desc
-				echo -n "$spaces"
-				echo -e "[\e[00;31m$status\e[00m]"
-			else
-				echo -n $desc
-				echo -n "$spaces"
-				echo -e "[\e[1;32m$status\e[00m]"
-			fi
+	#
+	# here identify the type of the check
+	# if is not identified pass to next or omitted it.
+	#
+	if [[ $type == 'config' || $type == 'process' || $type == 'file' ]]; then         
+		if [[ $status == 'NOT' ]]; then
+			echo "$(space_fill 80 right $desc)[$(color_string red FAIL)]"
 		else
-			echo "$desc ${spaces}[$status]"
+			echo "$(space_fill 80 right $desc)[$(color_string green PASS)]"
 		fi
 	fi
 done
